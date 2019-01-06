9c10d90bc6796d6d736175d2842b60ed185acec2
Natalia Jędrosz <n.jedrosz@gmail.com>
2019-01-06 22:02:10 +0100

    Done

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '87682452209-ni6bnvusib450al2okas9kag39qookvd.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'x_ufCYTotkpJvZXQ3F2wX83X',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback' 
+};
